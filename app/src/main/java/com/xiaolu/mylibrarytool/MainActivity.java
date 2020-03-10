1f5734ca7bd0676b419732b9dfc44a7ce8fbba72
xiaolu3550 <xiaolu3550@qq.com>
2020-03-10 10:57:50 +0800

    更新文档

--- app/src/main/java/com/xiaolu/mylibrarytool/MainActivity.java
+++ app/src/main/java/com/xiaolu/mylibrarytool/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * 在Application中添加
+     **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
